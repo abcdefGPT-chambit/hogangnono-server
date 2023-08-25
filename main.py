888f570c7c71d5f111520db164f59d2abef1d66e
bbangjun <lucid9dawn@gmail.com>
2023-08-25 15:56:57 +0900

    feat(main.py): selenium 관련 라이브러리 import

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchWindowException
