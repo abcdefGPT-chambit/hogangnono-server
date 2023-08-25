f941da51b55114843f1b0303e4e544844636dd23
bbangjun <lucid9dawn@gmail.com>
2023-08-25 16:27:18 +0900

    feat(main.py): 원래 창으로 전환 및 메인 화면으로 이동

--- main.py
+++ main.py
@@ -37,6 +37,21 @@ for window_handle in driver.window_handles:
     if "Facebook" in driver.title:
         break
 
+time.sleep(0.5)
+
 driver.find_element(By.CSS_SELECTOR, "#email").send_keys(config.EMAIL) # 이메일 입력
 driver.find_element(By.CSS_SELECTOR, "#pass").send_keys(config.PASSWORD) # 비밀번호 입력
 driver.find_element(By.CSS_SELECTOR, "#loginbutton").click() # 로그인 버튼 클릭
+
+time.sleep(0.5)
+# 원래 창으로 전환(아래 오류 방지)
+# urllib3.exceptions.ProtocolError: ('Connection aborted.', ConnectionResetError(10054, '현재 연결은 원격 호스트에 의해 강제로 끊겼습니다', None, 10054, None))
+try:
+    driver.switch_to.window(original_window)
+except NoSuchWindowException:
+    pass
+
+time.sleep(1)
+
+driver.get(config.HOGANGNONO_MAIN_URL) # 메인 화면으로 이동
+time.sleep(1)
