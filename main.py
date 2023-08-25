2f4864eab170d2a7250296ec4fd0fa03c050641d
bbangjun <lucid9dawn@gmail.com>
2023-08-25 15:57:38 +0900

    feat(main.py): 네트워크 환경을 고려한 time 설정을 위한 라이브러리 import

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import NoSuchWindowException
+import time
