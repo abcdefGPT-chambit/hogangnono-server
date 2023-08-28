533cc8ad1d3ca7763447fe3497049ad9568f90ca
bbangjun <lucid9dawn@gmail.com>
2023-08-28 13:42:11 +0900

    refactor: directory 구조 리팩토링

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchWindowException, StaleElementReferenceException, NoSuchElementException
 import time
-import config
+from config import config
 import json
 
 def initialize_driver():
