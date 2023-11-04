7357e6de54c2552cd973e64aaa977c3a23c59b3c
bbangjun <lucid9dawn@gmail.com>
2023-11-05 03:23:34 +0900

    feat(app.py): CSV 파일의 데이터 구축 API 구현

--- app.py
+++ app.py
@@ -13,6 +13,34 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+# CSV 파일의 데이터를 DB에 구축
+@app.route('/insertdata', methods=['POST'])
+def web_insertdata():
+    review_df = pd.read_csv('dataset/20231105_apt_review.csv')
+    for index, row in review_df.iterrows():
+        review = AptReview(
+            apt_code=row['apt_code'],
+            category=row['category'],
+            review=row['review']
+        )
+        db.session.add(review)
+
+    trade_df = pd.read_csv('dataset/20231105_apt_trade.csv')
+    for index, row in trade_df.iterrows():
+        trade = AptTrade(
+            apt_code=row['apt_code'],
+            highest=row['highest'],
+            lowest=row['lowest'],
+            high_floor=row['high_floor'],
+            middle_floor=row['middle_floor'],
+            low_floor=row['low_floor'],
+            trade_trend=row['trade_trend']
+        )
+        db.session.add(trade)
+
+    db.session.commit()
+
+    return "{'status' : '200', 'message' : 'ok'}"
 
 
 if __name__ == '__main__':
