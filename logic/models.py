e967c4e08d0dccf04279130054ca894e4802a254
bbangjun <lucid9dawn@gmail.com>
2023-11-04 23:56:22 +0900

    feat(models.py): apt_review 테이블 정의

--- logic/models.py
+++ logic/models.py
@@ -5,3 +5,10 @@ class AptInfo(db.Model):
     __tablename__ = 'apt_info'
     apt_code = db.Column(db.String(255), primary_key=True)
     apt_name = db.Column(db.String(255))
+
+class AptReview(db.Model):
+    __tablename__ = 'apt_review'
+    id = db.Column(db.Integer, primary_key=True)  # 각 리뷰의 고유 ID
+    apt_code = db.Column(db.String(255), db.ForeignKey('apt_info.apt_code'))  # 외래 키로 설정
+    category = db.Column(db.BigInteger)
+    review = db.Column(db.String(255))
