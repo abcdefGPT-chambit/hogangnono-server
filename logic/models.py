eb0407398839ae595e76dc6d03c6b389861e867c
bbangjun <lucid9dawn@gmail.com>
2023-11-04 23:56:36 +0900

    feat(models.py): apt_trade 테이블 정의

--- logic/models.py
+++ logic/models.py
@@ -12,3 +12,14 @@ class AptReview(db.Model):
     apt_code = db.Column(db.String(255), db.ForeignKey('apt_info.apt_code'))  # 외래 키로 설정
     category = db.Column(db.BigInteger)
     review = db.Column(db.String(255))
+
+class AptTrade(db.Model):
+    __tablename__ = 'apt_trade'
+    id = db.Column(db.Integer, primary_key=True)  # 각 거래의 고유 ID
+    apt_code = db.Column(db.String(255), db.ForeignKey('apt_info.apt_code'))  # 외래 키로 설정
+    highest = db.Column(db.String(255))
+    lowest = db.Column(db.String(255))
+    high_floor = db.Column(db.String(255))
+    middle_floor = db.Column(db.String(255))
+    low_floor = db.Column(db.String(255))
+    trade_trend = db.Column(db.String(255))
